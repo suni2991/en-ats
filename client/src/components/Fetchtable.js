b42d1f24ff2755f818ffb85616090cfcf1ed6c4a
Fawzan Ansari <fawzan.ansari@enfuse-solutions.com>
2024-07-22 11:19:56 +0530

    [feat] - add token in fetchtable file

--- client/src/components/Fetchtable.js
+++ client/src/components/Fetchtable.js
@@ -1,37 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import DataTable from 'react-data-table-component';
-import axios from 'axios';
-import CustomStyles from './CustomStyles';
+import React, { useState, useEffect } from "react";
+import DataTable from "react-data-table-component";
+import axios from "axios";
+import CustomStyles from "./CustomStyles";
 import { MdOutlineDownload } from "react-icons/md";
-import * as XLSX from 'xlsx';
-import { saveAs } from 'file-saver';
-import { Button } from 'antd';
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
+import { Button } from "antd";
+import useAuth from "../hooks/useAuth";
 
 // Utility function to capitalize the first letter of each word
 const capitalizeWords = (str) => {
   return str.replace(/\b\w/g, (char) => char.toUpperCase());
 };
 
-const token = process.env.REACT_APP_JWT_TOKEN
-
-const Fetchtable = ({ url, columns, title, onViewClick, filteredData, extraContent }) => {
+const Fetchtable = ({
+  url,
+  columns,
+  title,
+  onViewClick,
+  filteredData,
+  extraContent,
+}) => {
   const [data, setData] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [selectedRow, setSelectedRow] = useState(null);
+
+  const { token } = useAuth();
 
   useEffect(() => {
     if (!filteredData) {
       const fetchData = async () => {
         try {
-          const response = await axios.get(url,
-            {
-              headers:{
-                Authorization: `Bearer ${token}`
-              }
-            });
+          const response = await axios.get(url, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
           setData(response.data.reverse());
         } catch (error) {
-          console.error('Error fetching data:', error);
+          console.error("Error fetching data:", error);
         }
       };
 
@@ -39,9 +46,7 @@ const Fetchtable = ({ url, columns, title, onViewClick, filteredData, extraConte
     } else {
       setData(filteredData.reverse());
     }
-  }, [url, filteredData]);
-
-
+  }, [url, filteredData, token]);
 
   const handleSearch = (event) => {
     setSearchQuery(event.target.value);
@@ -49,12 +54,14 @@ const Fetchtable = ({ url, columns, title, onViewClick, filteredData, extraConte
 
   const filteredResults = data.filter((item) => {
     const query = searchQuery.toLowerCase();
-    const fullName = item.fullName ? item.fullName.toLowerCase() : '';
-    const position = item.position ? item.position.toLowerCase() : '';
-    const location = item.location ? item.location.toLowerCase() : '';
-    const postedBy = item.postedBy ? item.postedBy.toLowerCase() : '';
-    const role = item.role ? item.role.toLowerCase() : '';
-    const selectedCategory = item.selectedCategory ? item.selectedCategory.toLowerCase() : '';
+    const fullName = item.fullName ? item.fullName.toLowerCase() : "";
+    const position = item.position ? item.position.toLowerCase() : "";
+    const location = item.location ? item.location.toLowerCase() : "";
+    const postedBy = item.postedBy ? item.postedBy.toLowerCase() : "";
+    const role = item.role ? item.role.toLowerCase() : "";
+    const selectedCategory = item.selectedCategory
+      ? item.selectedCategory.toLowerCase()
+      : "";
     return (
       fullName.includes(query) ||
       position.includes(query) ||
@@ -72,40 +79,53 @@ const Fetchtable = ({ url, columns, title, onViewClick, filteredData, extraConte
   const handleExportToExcel = (dataToExport) => {
     const filteredData = filterDataForExport(dataToExport);
 
-    const capitalizedHeaders = Object.keys(filteredData[0] || {}).reduce((acc, key) => {
-      acc[capitalizeWords(key.replace(/_/g, ' '))] = key;
-      return acc;
-    }, {});
+    const capitalizedHeaders = Object.keys(filteredData[0] || {}).reduce(
+      (acc, key) => {
+        acc[capitalizeWords(key.replace(/_/g, " "))] = key;
+        return acc;
+      },
+      {}
+    );
 
-    const formattedData = filteredData.map(item => {
+    const formattedData = filteredData.map((item) => {
       return Object.keys(item).reduce((acc, key) => {
-        acc[capitalizeWords(key.replace(/_/g, ' '))] = item[key];
+        acc[capitalizeWords(key.replace(/_/g, " "))] = item[key];
         return acc;
       }, {});
     });
 
     const worksheet = XLSX.utils.json_to_sheet(formattedData);
     const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
-    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-    const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
-    saveAs(blob, 'data.xlsx');
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    const excelBuffer = XLSX.write(workbook, {
+      bookType: "xlsx",
+      type: "array",
+    });
+    const blob = new Blob([excelBuffer], { type: "application/octet-stream" });
+    saveAs(blob, "data.xlsx");
   };
 
   return (
-    <div className='fetch-table'>
-      <div className='search-cont'>
-        {extraContent && <div style={{ float: 'left' }}>{extraContent}</div>}
+    <div className="fetch-table">
+      <div className="search-cont">
+        {extraContent && <div style={{ float: "left" }}>{extraContent}</div>}
         <input
           type="text"
           placeholder="Search by FullName/ Job Title or Location"
           value={searchQuery}
           onChange={handleSearch}
-          style={{ float: 'left', width: '50%', padding: '6px', margin: '2px' }}
+          style={{ float: "left", width: "50%", padding: "6px", margin: "2px" }}
         />
-        <Button 
-          style={{ background: '#A60808', margin: '0px', color: '#FFF', float: 'right' }} 
-          onClick={() => handleExportToExcel(filteredResults.length ? filteredResults : data)}
+        <Button
+          style={{
+            background: "#A60808",
+            margin: "0px",
+            color: "#FFF",
+            float: "right",
+          }}
+          onClick={() =>
+            handleExportToExcel(filteredResults.length ? filteredResults : data)
+          }
         >
           <MdOutlineDownload /> Download Excel
         </Button>
