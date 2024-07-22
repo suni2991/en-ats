c5cd13bba894d89b502d952dd5a018d27b1ddf8c
Fawzan Ansari <fawzan.ansari@enfuse-solutions.com>
2024-07-22 11:18:36 +0530

    [feat] - remove token while logout

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,20 +1,18 @@
-import React, { useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { FaTh, FaAdn, FaRegUser } from 'react-icons/fa';
-import { MdLogout } from 'react-icons/md';
-import { TiBriefcase } from 'react-icons/ti';
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { FaTh, FaAdn, FaRegUser } from "react-icons/fa";
+import { MdLogout } from "react-icons/md";
+import { TiBriefcase } from "react-icons/ti";
 
-import { LuMonitorCheck } from 'react-icons/lu';
-import useAuth from '../hooks/useAuth';
-import '../styles/Sidebar.css';
-import logo from '../Assests/enfuse-logo.png';
-import { Tooltip, Button } from 'antd';
-import ProfilePage from './ProfilePage';
-import { VscFeedback } from 'react-icons/vsc';
+import { LuMonitorCheck } from "react-icons/lu";
+import useAuth from "../hooks/useAuth";
+import "../styles/Sidebar.css";
+import logo from "../Assests/enfuse-logo.png";
+import { Tooltip, Button } from "antd";
+import ProfilePage from "./ProfilePage";
+import { VscFeedback } from "react-icons/vsc";
 
-import Swal from 'sweetalert2';
-
-
+import Swal from "sweetalert2";
 
 const Sidebar = ({ children }) => {
   const navigate = useNavigate();
@@ -22,10 +20,7 @@ const Sidebar = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const [profileVisible, setProfileVisible] = useState(false);
-  const { auth, setAuth } = useAuth();
-
-
-
+  const { auth, setAuth, token, setToken } = useAuth();
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
@@ -33,70 +28,80 @@ const Sidebar = ({ children }) => {
 
   const logout = () => {
     setAuth({});
-    navigate('/');
+    navigate("/");
+    if (token) {
+      setToken(null);
+    }
     Swal.fire("You have been successfully logged out!!");
   };
 
   const menuItem = [
     {
-      path: '/dashboard',
-      name: 'Dashboard',
+      path: "/dashboard",
+      name: "Dashboard",
       icon: <LuMonitorCheck />,
     },
     {
-      path: '/jobdashboard',
-      name: 'Job Dashboard',
+      path: "/jobdashboard",
+      name: "Job Dashboard",
       icon: <LuMonitorCheck />,
     },
     {
-      path: '/admins',
-      name: 'EnFusians',
+      path: "/admins",
+      name: "EnFusians",
       icon: <FaAdn />,
     },
     {
-      path: '/jobs',
-      name: 'Jobs',
+      path: "/jobs",
+      name: "Jobs",
       icon: <TiBriefcase />,
     },
     {
-      path: '/hr',
-      name: 'ATS',
+      path: "/hr",
+      name: "ATS",
       icon: <FaRegUser />,
     },
     {
-      path: '/applicants',
-      name: 'Statistics',
+      path: "/applicants",
+      name: "Statistics",
       icon: <FaRegUser />,
     },
     {
-      path: '/reports',
-      name: 'Scores',
+      path: "/reports",
+      name: "Scores",
       icon: <FaTh />,
     },
 
     {
-      path: '/feedbacks',
-      name: 'Feedback',
+      path: "/feedbacks",
+      name: "Feedback",
       icon: <VscFeedback />,
     },
   ];
 
-
-
-
   return (
-
     <div className="container">
       {auth.role && (
-        <div style={{ width: isOpen ? '180px' : '25px' }} className="sidebar">
+        <div style={{ width: isOpen ? "180px" : "25px" }} className="sidebar">
           <div className="top-section">
-            <button style={{ background: 'none', border: 'none', float: 'left' }} onClick={toggleDarkMode}>
+            <button
+              style={{ background: "none", border: "none", float: "left" }}
+              onClick={toggleDarkMode}
+            >
               <img src={logo} alt="EnFuse" />
             </button>
-            {auth.role === 'Admin' || auth.role === 'Enfusian' ? (
+            {auth.role === "Admin" || auth.role === "Enfusian" ? (
               <img
-                style={{ width: '45%', borderRadius: '50%', background: 'white' }}
-                src={auth.image !== '' ? `http://localhost:5040${auth.image}` : require('../Assests/User.png')}
+                style={{
+                  width: "45%",
+                  borderRadius: "50%",
+                  background: "white",
+                }}
+                src={
+                  auth.image !== ""
+                    ? `http://localhost:5040${auth.image}`
+                    : require("../Assests/User.png")
+                }
                 alt="logo"
               />
             ) : null}
@@ -104,45 +109,67 @@ const Sidebar = ({ children }) => {
           </div>
 
           {menuItem.map((item, index) => {
-            if (auth.role === 'Admin') {
+            if (auth.role === "Admin") {
               return (
-                (item.name === 'EnFusians' || item.name === 'Statistics' || item.name === 'Dashboard') && (
-                  <NavLink to={item.path} key={index} className="link" activeclassname="active">
+                (item.name === "EnFusians" ||
+                  item.name === "Statistics" ||
+                  item.name === "Dashboard") && (
+                  <NavLink
+                    to={item.path}
+                    key={index}
+                    className="link"
+                    activeclassname="active"
+                  >
                     <div className="icon">{item.icon}</div>
                     <div className="link-text">{item.name}</div>
                   </NavLink>
                 )
               );
-            } else if (auth.role === 'HR') {
+            } else if (auth.role === "HR") {
               return (
-                (
-                  item.name === 'ATS' ||
-                  item.name === 'Dashboard' ||
-                  item.name === 'Scores' ||
-                  item.name === 'Statistics' ||
-                  item.name === 'Feedback') && (
-                  <NavLink to={item.path} key={index} className="link" activeclassname="active">
+                (item.name === "ATS" ||
+                  item.name === "Dashboard" ||
+                  item.name === "Scores" ||
+                  item.name === "Statistics" ||
+                  item.name === "Feedback") && (
+                  <NavLink
+                    to={item.path}
+                    key={index}
+                    className="link"
+                    activeclassname="active"
+                  >
                     <div className="icon">{item.icon}</div>
                     <div className="link-text">{item.name}</div>
                   </NavLink>
                 )
               );
-            } else if (auth.role === 'Panelist') {
+            } else if (auth.role === "Panelist") {
               return (
-                item.name === 'Feedback' && (
-                  <NavLink to={item.path} key={index} className="link" activeclassname="active">
+                item.name === "Feedback" && (
+                  <NavLink
+                    to={item.path}
+                    key={index}
+                    className="link"
+                    activeclassname="active"
+                  >
                     <div className="icon">{item.icon}</div>
                     <div className="link-text">{item.name}</div>
                   </NavLink>
                 )
               );
-            } else if (auth.role === 'Ops-Manager') {
-              return (item.name === 'Dashboard' || item.name === 'Feedback') && (
-                <NavLink to={item.path} key={index} className="link" activeclassname="active">
-                  <div className="icon">{item.icon}</div>
-                  <div className="link-text">{item.name}</div>
-                </NavLink>
-
+            } else if (auth.role === "Ops-Manager") {
+              return (
+                (item.name === "Dashboard" || item.name === "Feedback") && (
+                  <NavLink
+                    to={item.path}
+                    key={index}
+                    className="link"
+                    activeclassname="active"
+                  >
+                    <div className="icon">{item.icon}</div>
+                    <div className="link-text">{item.name}</div>
+                  </NavLink>
+                )
               );
             } else {
               return null;
@@ -150,14 +177,21 @@ const Sidebar = ({ children }) => {
           })}
         </div>
       )}
-      <div className='main-container'>
+      <div className="main-container">
         <main>
           <div>
-            {(auth.role === 'HR' || auth.role === 'Admin' || auth.role === 'Panelist' || auth.role === 'Ops-Manager') && (
+            {(auth.role === "HR" ||
+              auth.role === "Admin" ||
+              auth.role === "Panelist" ||
+              auth.role === "Ops-Manager") && (
               <nav className="navbar">
                 <div className="navbar-right">
-                  <Tooltip title="View & Update Profile" color='cyan'>
-                    <Button type="text" onClick={() => setProfileVisible(true)} style={{ color: '#00B4D2' }}>
+                  <Tooltip title="View & Update Profile" color="cyan">
+                    <Button
+                      type="text"
+                      onClick={() => setProfileVisible(true)}
+                      style={{ color: "#00B4D2" }}
+                    >
                       Welcome, {auth.fullName} |
                     </Button>
                   </Tooltip>
@@ -167,29 +201,31 @@ const Sidebar = ({ children }) => {
                   <button className="icon-button" onClick={logout}>
                     <span className="icon-container">
                       <MdLogout />
-                    </span>{' '}
+                    </span>{" "}
                     <span className="text">Logout</span>
                   </button>
                 )}
               </nav>
             )}
-            {auth && <ProfilePage open={profileVisible} auth={auth} setAuth={setAuth} onClose={() => setProfileVisible(false)} />}
+            {auth && (
+              <ProfilePage
+                open={profileVisible}
+                auth={auth}
+                setAuth={setAuth}
+                onClose={() => setProfileVisible(false)}
+              />
+            )}
           </div>
-          <div className='main-content'>
-            {children}
-          </div>
+          <div className="main-content">{children}</div>
 
           {auth.role && (
             <div className="footer">
               @ 2024 EnFuse Solutions. All rights Reserved
             </div>
-
           )}
         </main>
       </div>
     </div>
-
-
   );
 };
 
